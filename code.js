df846c96ea5a2dc13111b6fb9673384985559d08
mjschrader307 <mjschrader307@gmail.com>
2025-02-06 15:57:55 -0700

    Done; ready for review

--- code.js
+++ code.js
@@ -1,3 +1,25 @@
+function search(list, element, low, high) {
+  // Element not in array case
+  if (low > high) return -1;
+
+  let mid = Math.floor((low + high) / 2);
+
+  if (list[mid] === element) return mid;
+
+  // Recursive case
+
+  if (list[mid] > element) {
+    // Element in left half
+    return search(list, element, low, mid - 1);
+  } else if (list[mid] < element) {
+    // Element in right half
+    return search(list, element, mid + 1, high);
+  }
+}
+
 function binarySearch(list, element) {
-    return -1;
+  let low = 0;
+  let high = list.length - 1;
+
+  return search(list, element, low, high);
 }
